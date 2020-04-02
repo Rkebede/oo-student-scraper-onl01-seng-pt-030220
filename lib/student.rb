d92d87a8f8dcc7e5fb341321dd302a7e735c5151
Rkebede <rutht.kebede@gmail.com>
2020-04-02 02:06:22 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,29 +5,25 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    # student_hash.each {|element, value| self.send(("#{element}="), value)}
-    @name          = student_hash[:name]
-    @location      = student_hash[:location]
-    @twitter       = student_hash[:twitter]
-    @linkedin      = student_hash[:linkedin]
-    @github        = student_hash[:github] 
-    @blog          = student_hash[:blog]
-    @profile_quote = student_hash[:profile_quote]
-    @bio           = student_hash[:bio]
-    @profile_url   = student_hash[:profile_url]
+    student_hash.each { |attributes, value| self.send(("#{attributes}="), value)}
+    # @name          = student_hash[:name]
+    # @location      = student_hash[:location]
+    # @twitter       = student_hash[:twitter]
+    # @linkedin      = student_hash[:linkedin]
+    # @github        = student_hash[:github] 
+    # @blog          = student_hash[:blog]
+    # @profile_quote = student_hash[:profile_quote]
+    # @bio           = student_hash[:bio]
+    # @profile_url   = student_hash[:profile_url]
     @@all << self
   end 
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash|
-      self.new(student_hash)
-    end
+    students_array.each { |student_hash| self.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attribute, value|
-      self.send(("#{attribute}="), value)
-    end 
+    attributes_hash.each { |attribute, value| self.send(("#{attribute}="), value) } 
   end
 
   def self.all
